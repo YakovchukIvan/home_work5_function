90534ad2145f4acb590650bd79af37b8168c2e09
litvinovjobs@gmail.com <litvinovjobs@gmail.com>
2023-03-02 23:12:46 +0200

    codding 2 and 3 task

--- script.js
+++ script.js
@@ -1,77 +1,15 @@
 "use strict"
 
-// 1. Функція приймає масив і виводить його на екран.
-
-// const arr_1 = [];
-// const ul = document.createElement("ul"); // створюємо елемент u
-// document.body.appendChild(ul); // поки не знаю що це 
-
-// function returnArray() {
-//     for (let i = 0; i < 10; i++ ) {
-//         arr.push(Math.round( Math.random() * 100 )); 
-//     }
-//     return arr;
-// }  // створює рандомний масив
-
-// const newArr = returnArray(); 
-
-// let result = newArr.map((elem) => {
-//     return `
-//     <li>${elem}</li>
-//     `
-// }).join('');
-
-// ul.innerHTML = result;
-
-
-
-/* 
-    2. Функція приймає масив і виводить лише парні елементи.
-*/
-
-
-// const ul = document.createElement("ul"); // створюємо елемент u
-// document.body.appendChild(ul); // поки не знаю що це 
-
-// function returnArray() {
-//     for (let i = 0; i < 10; i++ ) {
-//         arr1.push(Math.round( Math.random() * 100 )); 
-//     }
-//     return arr1;
-// }  // створює рандомний масив
-
-
-// const arr_numbers42 = [-42,45,-342,65,687,-345,231,-465,23,234,-56,42,2,54,6];
-// const filterOdd = arr_numbers42.filter(arr => arr % 2 === 0 && arr > 0)
-// console.log(filterOdd);
-
-
-// const arr1 = [];
-// console.log(arr1);
-// const test = arr1.filter(arr1 => (arr1 % 2 === 0))
-// console.log("test", test);
-
-
-// const newArr = returnArray(); 
-
-// let result = newArr.map((elem) => {
-//     return `
-//     <li>${elem}</li>
-//     `
-// }).join('');
-
-// ul.innerHTML = result;
-
-
-
 //  1. Створіть функцію, яка повертає випадкове число
 
-const taskText = document.createElement("p");
-const buttonTask = document.createElement("button")
-const ul = document.createElement("ul"); // створюємо елемент ul для JS
-document.body.appendChild(taskText); // цією командою вже створюємо самий елемент в body
-document.body.appendChild(buttonTask); 
-document.body.appendChild(ul);
+// const taskText = document.createElement("p");
+// const buttonTask = document.createElement("button")
+// const ul = document.createElement("ul"); // створюємо елемент ul для JS
+// document.body.appendChild(taskText); // цією командою вже створюємо самий елемент в body
+// document.body.appendChild(buttonTask); 
+// document.body.appendChild(ul); 
+
+// це вище не потрібно, лише для тестування тут)
 
 let number1= 0;
 
@@ -85,3 +23,74 @@ document.querySelector("button").onclick = function(){
 ul.innerHTML = number1;
 taskText.innerText = "1. Створіть функцію, яка повертає випадкове число";
 buttonTask.innerText = "Для початку функції нажміть тут";
+
+
+
+// 2. Створіть функцію, яка приймає дві змінні та міняє їх значення місцями
+
+
+const valueForm = document.querySelector('#value-form')
+const contentBlock = document.querySelector('#task_2')
+let temp;
+const valueUser = [];
+
+valueForm.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    const formData = new FormData(valueForm)
+    const oneValue = formData.get("oneValue")
+    const twoValue = formData.get('twoValue')
+
+    const data ={
+        oneValue,
+        twoValue,
+    }
+    valueUser.push(data)
+
+    render(contentBlock, valueUser)
+})
+
+function render(element, data){
+    element.innerHTML = data.map(({oneValue ,twoValue}) => {
+        temp = oneValue;
+        oneValue = twoValue;
+        twoValue = temp;
+        return `
+            <p>${oneValue}</p>
+            <p>${twoValue}</p>
+        `
+    }).join(' ')
+}
+
+
+
+// 3. Створіть функцію, яка приймає число та повертає істину, якщо воно парне
+
+
+const numberForm = document.querySelector('#number-form')
+const contentBlock_2 = document.querySelector('#task_3')
+const numberUser = [];
+
+numberForm.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    const formData = new FormData(numberForm)
+    const enterNumber = formData.get("enterNumber")
+
+    const data ={
+        enterNumber
+    }
+    numberUser.push(data)
+
+    render(contentBlock_2, numberUser)
+})
+
+function render(element, data){
+    element.innerHTML = data.map(({enterNumber}) => {
+        if (enterNumber % 2 == 0){
+            return `<p>${enterNumber} Істина</p>`
+        } else return `<p>${enterNumber} Не істина</p>`
+
+        // return `
+        //     <p>${enterNumber}</p>
+        // `
+    }).join(' ')
+}
